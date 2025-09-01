9fbadb782e22b7037f46b80e1fc70d857c8ef432
Omar Badawy <omar.badawy@hotmail.com>
2025-09-01 14:55:10 +0200

    updated readme

---
+++ src/user_auth/user_auth.module.ts
@@ -0,0 +1,14 @@
+// import { Module } from '@nestjs/common';
+// import { SequelizeModule } from '@nestjs/sequelize';
+// import {
+
+// } from './models';
+
+// @Module({
+//   imports: [
+//     SequelizeModule.forFeature([
+
+//     ]),
+//   ],
+// })
+// export class WhoopModule {}
