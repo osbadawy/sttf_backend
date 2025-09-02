2b922ffc6dee29159a8fbedaa517e1206d7e3df7
Omar Badawy <omar.badawy@hotmail.com>
2025-09-02 18:16:32 +0200

    feat:auth added

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Global, Module } from '@nestjs/common';
+import { FirebaseAdminProvider, FIREBASE_ADMIN } from './firebase-admin.provider';
+import { FirebaseAuthGuard } from './firebase-auth.guard';
+import {TestAuthController} from './test.controller'
+
+@Global()
+@Module({
+  providers: [FirebaseAdminProvider, FirebaseAuthGuard],
+  exports:   [FirebaseAuthGuard, FIREBASE_ADMIN],
+  controllers: [TestAuthController]
+})
+export class AuthModule {}
---
+++ src/auth/firebase-auth.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import type * as admin from 'firebase-admin';
+import { FIREBASE_ADMIN } from './firebase-admin.provider';
+
+@Injectable()
+export class FirebaseAuthGuard implements CanActivate {
+  constructor(@Inject(FIREBASE_ADMIN) private readonly fb: typeof admin) {}
+
+  async canActivate(ctx: ExecutionContext): Promise<boolean> {
+    const req = ctx.switchToHttp().getRequest();
+    const auth = String(req.headers.authorization || '');
+    const token = auth.startsWith('Bearer ') ? auth.slice(7) : '';
+
+    if (!token) throw new UnauthorizedException('Missing Bearer token');
+
+    try {
+      const decoded = await this.fb.auth().verifyIdToken(token, true); // true checks revocation
+      req.user = { uid: decoded.uid, email: decoded.email, claims: decoded };
+      return true;
+    } catch (e: any) {
+      throw new UnauthorizedException(e?.message || 'Invalid Firebase token');
+    }
+  }
+}
