3bf2ad0cff0c099c5f395bce3ef11e5509a1c32d
AmNat789 <amit.amanna@code.berlin>
2025-09-09 11:11:05 +0200

    feat: implement WhoopAppController for handling single and multi-day workout summaries

---
+++ src/whoop/controllers/app.controller.ts
@@ -0,0 +1,27 @@
+// whoop.controller.ts
+import { Controller, UseGuards, Post, Body, Req, Get } from '@nestjs/common';
+import type { Request } from 'express';
+import { WhoopUserService, WhoopWorkoutService } from 'src/whoop/services';
+import { FirebaseAuthGuard } from 'src/auth/firebase-auth.guard';
+import { WhoopAppMultiDayRequest, WhoopAppSingleDayRequest } from 'src/whoop/dtos';
+
+
+@Controller('whoop/app')
+export class WhoopAppController {
+  constructor(
+    private readonly whoopWorkoutService: WhoopWorkoutService,
+    private readonly whoopUserService: WhoopUserService,
+  ) {}
+
+  @UseGuards(FirebaseAuthGuard)
+  @Get("/day")
+  async day(@Req() req: Request & { user: { uid: string } }, @Body() body: WhoopAppSingleDayRequest) {
+    return await this.whoopUserService.getDaySummary(body.user_id, body.day);
+  }
+
+  @UseGuards(FirebaseAuthGuard)
+  @Get("/workout")
+  async workout(@Req() req: Request, @Body() body: WhoopAppMultiDayRequest) {
+    return await this.whoopWorkoutService.getWorkouts(body.user_id, body.days);
+  }
+}
